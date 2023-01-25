bcdc2f288232d41ed4238f8517d11ddea5278c25
Strahinja <sljubicic7120rn@raf.rs>
2023-01-25 01:29:04 +0100

    Dodata prva strana

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,12 +1,48 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
+    <div>
+      <b-navbar toggleable="sm" type="light" variant="light">
+        <b-navbar-brand to="/">
+          <img
+            src="./assets/gem.png"
+            class="d-inline-block align-top"
+            alt="Diamond"
+          />
+          Zlatara Ljubicic
+        </b-navbar-brand>
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+        <b-collapse id="nav-collapse" is-nav>
+          <b-navbar-nav class="ms-auto">
+            <b-nav-item to="/">Home</b-nav-item>
+            <b-nav-item-dropdown text="Products">
+              <b-dropdown-item> "ENGLESKI" </b-dropdown-item>
+            </b-nav-item-dropdown>
+            <b-nav-item to="/contact">Contact</b-nav-item>
+            <b-nav-item to="/yourcart">Cart</b-nav-item>
+            <b-nav-item to="/signin">Sign In</b-nav-item>
+            <b-nav-item to="/signup">Sign Up</b-nav-item>
+          </b-navbar-nav>
+        </b-collapse>
+      </b-navbar>
+    </div>
+    <router-view />
   </div>
 </template>
+
+<script>
+export default {
+  name: "App",
+
+  data() {
+    return {
+      searchQuery: "",
+    };
+  },
+
+  methods: {},
+};
+</script>
 
 <style>
 #app {
@@ -14,19 +50,25 @@
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #000000;
 }
 
 nav {
   padding: 30px;
+  color: #000000;
 }
 
 nav a {
   font-weight: bold;
-  color: #2c3e50;
+  color: #000000;
 }
 
 nav a.router-link-exact-active {
-  color: #42b983;
+  color: #000000;
+}
+
+#nav-collapse {
+  margin-right: 40px;
+  color: #000000;
 }
 </style>
--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,17 +1,29 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-  },
-  getters: {
+    products: [],
+    productTypes: [],
   },
   mutations: {
+    addProducts(state, product) {
+      state.products.push(product);
+    },
+
+    addProductType(state, productType) {
+      state.productTypes = productType;
+    },
   },
   actions: {
+    fetchProducts({ commit }) {
+      fetch("localhost:8000/admin/products/", {
+        method: "GET",
+      })
+        .then((obj) => obj.json())
+        .then((res) => commit("addProducts", res.products));
+    },
   },
-  modules: {
-  }
-})
+});
